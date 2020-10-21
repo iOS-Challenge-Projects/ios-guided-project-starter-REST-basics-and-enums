56c26d67d1d719249a0e844028db41a6c66a411d
fritzgt <tuanis@live.com>
2020-10-21 14:46:50 +0300

    building url

--- FindACrew/Model Controllers/PersonController.swift
+++ FindACrew/Model Controllers/PersonController.swift
@@ -15,12 +15,35 @@ class PersonController {
         case post = "POST"
         case delete = "DELETE"
     }
-    
-    private let baseURL = URL(string: "https://lambdaswapi.herokuapp.com")!
-    private lazy var peopleURL = URL(string: "/api/people", relativeTo: baseURL)!
     var people:[Person] = []
     
+    //1.Base URL
+    private let baseURL = URL(string: "https://lambdaswapi.herokuapp.com")!
+    
+    //2.Add directory base on documentations
+    private lazy var peopleURL = URL(string: "/api/people", relativeTo: baseURL)!
+    
+    
+    //Request data functions
     func searchForPeople(searchTerm: String, completion: @escaping () -> Void) {
+        
+        
+        //Create a component to build a query url using the base url
+        var urlComponents = URLComponents(url: peopleURL, resolvingAgainstBaseURL: true)
+        
+        //create a query item
+        let searchTermQueryItem = URLQueryItem(name: "search", value: searchTerm)
+        
+        //Add it to the component = "https://lambdaswapi.herokuapp.com?search:anyname"
+        urlComponents?.queryItems = [searchTermQueryItem]
+        
+        //Unwrap url
+        guard let requestURL = urlComponents?.url else {
+            print("Request URL nil")
+            completion()//Call completion to let user know of an error
+            return
+        }
+        
         
     }
 }
